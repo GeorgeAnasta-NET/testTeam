a6bd3d688e1965c1f1b062da53ea0b72a2cd875d
Giorgos Neuroleak <infidel6@hotmail.com>
2019-05-31 20:14:50 +0300

    Add project files.

---
+++ testTeam/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace testTeam
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
